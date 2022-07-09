7e0cace1bc5bffa67853af89a5864e37681f6a0d
DennisRono <dennisrkibet@gmail.com>
2022-07-09 15:36:00 +0300

    handsome rocket go film magic

--- wanjie.py
+++ wanjie.py
@@ -1,14 +1,21 @@
+# import os
+
+
+# # def files(dirname, filesize):
+# #     try:
+# #         files = list(filter(lambda x:os.path.getsize(x)>filesize, [os.path.join(dirname, x) for x in os.listdir(dirname)]))
+# #         return files
+# #     except:
+# #         print("an error ocured")
+
+# # print(files("venv", 200))
+
+# for x in os.listdir("venv"):
+#     print(x)
+
+import glob
 import os
 
-
-# def files(dirname, filesize):
-#     try:
-#         files = list(filter(lambda x:os.path.getsize(x)>filesize, [os.path.join(dirname, x) for x in os.listdir(dirname)]))
-#         return files
-#     except:
-#         print("an error ocured")
-
-# print(files("venv", 200))
-
-for x in os.listdir("venv"):
-    print(x)
+for fyle in glob.glob('*'):
+    if os.stat(fyle).st_size > 20000:
+        print fyle, os.stat(fyle).st_size
