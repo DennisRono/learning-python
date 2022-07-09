8c7f5ed531aa184b1f33e399b5b5df69f1e2ccc8
DennisRono <dennisrkibet@gmail.com>
2022-07-09 15:32:03 +0300

    grown season club write living

--- wanjie.py
+++ wanjie.py
@@ -3,8 +3,9 @@ import os
 
 def files(dirname, filesize):
     try:
-        filter(lambda x:os.path.getsize(x)>filesize, [os.path.join(dirname, x) for x in os.listdir(dirname)])
+        files = filter(lambda x:os.path.getsize(x)>filesize, [os.path.join(dirname, x) for x in os.listdir(dirname)])
+        return files
     except:
         print("an error ocured")
 
-files("venv", 2000)
+print(files("venv", 2000))
