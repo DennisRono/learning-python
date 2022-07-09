0371db3c3d546de07e67047335ba332ca2a8f05c
DennisRono <dennisrkibet@gmail.com>
2022-07-09 15:29:00 +0300

    pine mood party guide paper

--- wanjie.py
+++ wanjie.py
@@ -1,19 +1,6 @@
-# import os
+import os
 
-# def files(dirname, filesize):
-#     print([(os.path +file) for file in os.listdir(dirname) if os.path.getsize(file) > filesize])
-#     filter(lambda x:os.path.getsize>20000L, [os.path.join(dir, x) for x in os.listdir(dir)])
+def files(dirname, filesize):
+    filter(lambda x:os.path.getsize(x)>filesize, [os.path.join(dirname, x) for x in os.listdir(dirname)])
 
-# files("./venv", 2000)
-
-
-# a list contains both even and odd numbers. 
-seq = [0, 1, 2, 3, 5, 8, 13]
-  
-# result contains odd numbers of the list
-result = filter(lambda x: x % 2 != 0, seq)
-print(list(result))
-  
-# result contains even numbers of the list
-result = filter(lambda x: x % 2 == 0, seq)
-print(list(result))
+files("venv", 2000)
