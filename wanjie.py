0bf32b904e6365b7311a8fad16ed05413ef89351
DennisRono <dennisrkibet@gmail.com>
2022-07-09 15:17:00 +0300

    hurt animal possibly last hide

--- wanjie.py
+++ wanjie.py
@@ -1,4 +1,4 @@
 import os
 
 def files(dirname, filesize):
-    [(path +file) for file in os.listdir(dirname) if os.path.getsize(file) > filesize]
+    print([(os.path +file) for file in os.listdir(dirname) if os.path.getsize(file) > filesize])
