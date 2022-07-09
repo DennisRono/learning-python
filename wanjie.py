c2ed56c60595f020fc0bd7121475b3865c3a956b
DennisRono <dennisrkibet@gmail.com>
2022-07-09 15:31:00 +0300

    follow gray product book lower

--- wanjie.py
+++ wanjie.py
@@ -1,6 +1,10 @@
 import os
 
+
 def files(dirname, filesize):
-    filter(lambda x:os.path.getsize(x)>filesize, [os.path.join(dirname, x) for x in os.listdir(dirname)])
+    try:
+        filter(lambda x:os.path.getsize(x)>filesize, [os.path.join(dirname, x) for x in os.listdir(dirname)])
+    except:
+        print("an error ocured")
 
 files("venv", 2000)
