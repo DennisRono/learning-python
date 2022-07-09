e92e1f476616a1813c828cc100609a2c084665b2
DennisRono <dennisrkibet@gmail.com>
2022-07-09 15:35:00 +0300

    product key wool apartment catch

--- wanjie.py
+++ wanjie.py
@@ -1,11 +1,14 @@
 import os
 
 
-def files(dirname, filesize):
-    try:
-        files = list(filter(lambda x:os.path.getsize(x)>filesize, [os.path.join(dirname, x) for x in os.listdir(dirname)]))
-        return files
-    except:
-        print("an error ocured")
+# def files(dirname, filesize):
+#     try:
+#         files = list(filter(lambda x:os.path.getsize(x)>filesize, [os.path.join(dirname, x) for x in os.listdir(dirname)]))
+#         return files
+#     except:
+#         print("an error ocured")
 
-print(files("venv", 2000))
+# print(files("venv", 200))
+
+for x in os.listdir("venv"):
+    print(x)
