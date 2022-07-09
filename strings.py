21ba9c3d48256257e8d4d88ec77afbacbdccc87a
DennisRono <dennisrkibet@gmail.com>
2022-07-09 17:11:01 +0300

    while tank alone arrange bottle

--- strings.py
+++ strings.py
@@ -6,5 +6,5 @@ last_name = "Kyle"
 
 # formatted strings
 
-print(f"my first name is {first_name} and my last name is {last_name}")
-print(first_name.capitalize())
+# print(f"my first name is {first_name} and my last name is {last_name}")
+print(first_name.capitalize().join(last_name.capitalize()," "))
