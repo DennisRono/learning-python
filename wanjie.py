6a0aa30352879b9490482fd5c22f55e0dedd71a4
DennisRono <dennisrkibet@gmail.com>
2022-07-09 15:34:00 +0300

    rest route railroad flag figure

--- wanjie.py
+++ wanjie.py
@@ -3,7 +3,7 @@ import os
 
 def files(dirname, filesize):
     try:
-        files = filter(lambda x:os.path.getsize(x)>filesize, [os.path.join(dirname, x) for x in os.listdir(dirname)])
+        files = list(filter(lambda x:os.path.getsize(x)>filesize, [os.path.join(dirname, x) for x in os.listdir(dirname)]))
         return files
     except:
         print("an error ocured")
