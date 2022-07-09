3cec3bfd3a51901a17eec684d220c445346e678e
DennisRono <dennisrkibet@gmail.com>
2022-07-09 15:59:00 +0300

    rod zebra sides contrast would

--- wanjie.py
+++ wanjie.py
@@ -1,26 +1,14 @@
 import os
 
-
-# # def files(dirname, filesize):
-# #     try:
-# #         files = list(filter(lambda x:os.path.getsize(x)>filesize, [os.path.join(dirname, x) for x in os.listdir(dirname)]))
-# #         return files
-# #     except:
-# #         print("an error ocured")
-
-# # print(files("venv", 200))
-
-def main():
-    dirName = 'venv';
+def filterFileBySize(dirname, filesize):
     listOfFiles = list()
-    for (dirpath, dirnames, filenames) in os.walk(dirName):
+    for (dirpath, dirnames, filenames) in os.walk(dirname):
         listOfFiles += [os.path.join(dirpath, file) for file in filenames]
-    files = list(filter(lambda x:os.path.getsize(x)>2000, listOfFiles))
+    files = list(filter(lambda x:os.path.getsize(x)>filesize, listOfFiles))
     for r in files:
         f = open("results.txt", "a")
         f.write(r)
     f.close()
 
-    
-if __name__ == '__main__':
-    main()
+
+filterFileBySize("venv", 250000)
